51cd14cedfd9cba85ec9c367ef52b91b39071435
CWTWIS <chawanrat.wis@gmail.com>
2024-01-29 17:39:38 +0700

    register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import RegisterForm from "./layouts/RegisterForm";
 
 function App() {
   const [dark, setDark] = useState(false);
@@ -19,15 +20,7 @@ function App() {
         checked={dark}
       />
       <hr />
-      <div className="flex gap-2">
-        <button class="btn">Button</button>
-        <button class="btn btn-neutral">Neutral</button>
-        <button class="btn btn-primary">Primary</button>
-        <button class="btn btn-secondary">Secondary</button>
-        <button class="btn btn-accent">Accent</button>
-        <button class="btn btn-ghost">Ghost</button>
-        <button class="btn btn-link">Link</button>
-      </div>
+      <RegisterForm />
     </div>
   );
 }
